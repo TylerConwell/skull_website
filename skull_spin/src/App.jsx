41fa18c6d15c2097ba714b5657534725b483ae7a
Tyler Conwell <wgc14@txstate.edu>
2025-12-29 14:04:57 -0600

    making some skull moving stuff

--- skull_spin/src/App.jsx
+++ skull_spin/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [skull_prt1, setSkullFirst] = useState("");
+  const [skull_prt2, setSkullSecond] = useState("");
+  const [skull_prt3, setSkullThird] = useState("");
+  const [skull_prt4, setSkullFourth] = useState("");
+
+
+  useEffect(() => {
+    fetch("/assets/setSkullFirst.txt")
+      .then(res => res.text())
+      .then(setSkullFirst);
+
+    fetch("/asset/setSkullSecond.txt")
+      .then(res => res.text())
+      .then(setSkullSecond);
+
+    fetch("/asset/setSkullThird.txt")
+      .then(res => res.text())
+      .then(setSkullThird);
+
+    fetch("/asset/setSkullFourth.txt")
+      .then(res => res.text())
+      .then(setSkullFourth);
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <pre className="skull">
+      {skull_prt1}
+      {skull_prt2}
+      {skull_prt3}
+      {skull_prt4}
+    </pre>
+  );
 }
 
 export default App
