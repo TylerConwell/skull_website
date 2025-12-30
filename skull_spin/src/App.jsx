04669a4ba24baf80dc086671fd12f0285b875615
Tyler Conwell <wgc14@txstate.edu>
2025-12-30 16:02:37 -0600

    making the ascii hands

--- skull_spin/src/App.jsx
+++ skull_spin/src/App.jsx
@@ -6,3 +6,5 @@ import skull_prt1 from './assets/skull_prt1.txt?raw';
 import skull_prt2 from './assets/skull_prt2.txt?raw';
 import skull_prt3 from './assets/skull_prt3.txt?raw';
 import skull_prt4 from './assets/skull_prt4.txt?raw';
+
+// import all the ascii hands as raw str's as well
