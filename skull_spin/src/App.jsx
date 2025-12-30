84e4fd00007fcfecec6ff8a68809817050a8e1ac
Tyler Conwell <wgc14@txstate.edu>
2025-12-30 15:37:59 -0600

    making the app.jsx file the correct way

--- skull_spin/src/App.jsx
+++ skull_spin/src/App.jsx
@@ -1,9 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-// import all the skull ascii files as a raw str for vite 
+// import all the ascii skulls as raw str's for vite
 import skull_prt1 from './assets/skull_prt1.txt?raw';
 import skull_prt2 from './assets/skull_prt2.txt?raw';
 import skull_prt3 from './assets/skull_prt3.txt?raw';
 import skull_prt4 from './assets/skull_prt4.txt?raw';
-
