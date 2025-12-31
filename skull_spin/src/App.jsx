3efd146764fb56a2c9a1084a01cc29a0550f92a8
Tyler Conwell <wgc14@txstate.edu>
2025-12-31 08:51:16 -0600

    finished hand 3 and continuing the code

--- skull_spin/src/App.jsx
+++ skull_spin/src/App.jsx
@@ -11,3 +11,9 @@ import skull_prt4 from './assets/skull_prt4.txt?raw';
 import hand_prt1 from './assets/hand_prt1.txt?raw';
 import hand_prt2 from './assets/hand_prt2.txt?raw';
 import hand_prt3 from './assets/hand_prt3.txt?raw';
+
+// adding the array for the skull ascii pics
+const all_skull_pics = [skull_prt1, skull_prt2, skull_prt3, skull_prt4];
+
+// adding the array for the hand ascii pics
+const all_hand_pics = [hand_prt1, hand_prt2, hand_prt3];
